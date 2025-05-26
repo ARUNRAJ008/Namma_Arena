998fb4e56868619589fc091eade34fd33e6539d5
Naga Harish <harinaga20@gmail.com>
2025-05-26 21:41:57 +0530

    project setup commit

---
+++ BackEnd/demo/src/main/java/com/example/na/NammaArenaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.na;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NammaArenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NammaArenaBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/demo/src/test/java/com/example/na/NammaArenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.na;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NammaArenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
